30a2a2dfecd9725e339c1cb9138c42fd75312992
Dashaa <d.smirnova@lab244.ru>
2025-05-21 17:37:20 +0300

    Rele Controller Ready

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controller_test/ReleControllerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controller_test/ReleControllerTest.java
@@ -1,27 +1,41 @@
 package org.firstinspires.ftc.teamcode.controller_test;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.*;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 @Config
+@TeleOp (name="Rele")
 public class ReleControllerTest extends LinearOpMode {
-    public static double TARGET = 100;
+    public static double TARGET = 2000;
     @Override
     public void runOpMode() throws InterruptedException {
         DcMotor motor = hardwareMap.get(DcMotor.class, "motor");
+
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+
+        Telemetry dashboardTelemetry = dashboard.getTelemetry();;
 
         motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
-        if (isStopRequested()) return;
-        if (motor.getCurrentPosition() < TARGET) {
-            motor.setPower(1);
-        } else if (motor.getCurrentPosition() > TARGET) {
-            motor.setPower(-1);
-        } else if (motor.getCurrentPosition() == TARGET) {
-            motor.setPower(0);
+
+        while (opModeIsActive()) {
+            if (motor.getCurrentPosition() < TARGET) {
+                motor.setPower(1);
+            } else if (motor.getCurrentPosition() > TARGET) {
+                motor.setPower(-1);
+            } else if (motor.getCurrentPosition() == TARGET) {
+                motor.setPower(0);
+            }
+            dashboardTelemetry.addData("Current position - ", motor.getCurrentPosition());
+            dashboardTelemetry.update();
         }
+
     }
 }
